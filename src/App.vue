2016362948e25ae1ebb4d57d092ebb9d5c1216b2
martahartawan <martahartawan81@gmail.com>
2022-07-11 15:09:54 +0800

    first comit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,97 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="container">
+    <Header
+      @toggle-add-task="toggleAddTask"
+      title="Task Tracker"
+      :showAddTask="showAddTask"
+    />
+    <div v-show="showAddTask">
+      <AddTask @add-task="addTask" />
+    </div>
+    <Tasks
+      @toggle-reminder="toggleReminder"
+      @delete-task="deleteTask"
+      :tasks="tasks"
+    />
+  </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Tasks,
+    AddTask,
+  },
+  data() {
+    return {
+      tasks: [],
+      showAddTask: false,
+    };
+  },
+  methods: {
+    toggleAddTask() {
+      this.showAddTask = !this.showAddTask;
+    },
+    async addTask(task) {
+      const res = await fetch(
+        "https://6283bcbc38279cef71dbafbe.mockapi.io/vuejs",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "aplication/json",
+            body: JSON.stringify(task),
+          },
+        }
+      );
+
+      const data = await res.json();
+      this.tasks = [...this.tasks, task];
+    },
+    async deleteTask(id) {
+      if (confirm("apakah anda ingin menghapus ?")) {
+        const res = await fetch(
+          "https://6283bcbc38279cef71dbafbe.mockapi.io/vuejs/${id}",
+          {
+            method: "DELETE",
+          }
+        );
+        res.status === 200
+          ? (this.tasks = this.tasks.filter((task) => task.id !== id))
+          : alert("Error delete")
+      }
+    },
+    toggleReminder(id) {
+      this.tasks = this.tasks.map(
+        task => (task.id === id ? { ...task, reminder: !task.reminder } : task)
+      );
+    },
+    async fetchTasks() {
+      const res = await fetch(
+        "https://6283bcbc38279cef71dbafbe.mockapi.io/vuejs"
+      );
+      const data = await res.json();
+      return data;
+    },
+    async fetchTask(id) {
+      const res = await fetch(
+        "https://6283bcbc38279cef71dbafbe.mockapi.io/vuejs/${id}"
+      );
+      const data = await res.json();
+      return data;
+    },
+  },
+  async created() {
+    this.tasks = await this.fetchTasks();
+  },
+};
+</script>
 
 <style>
 #app {
@@ -13,6 +100,56 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.btn:active {
+  transform: scale(0.98);
+}
+
+.btn-block {
+  display: block;
+  width: 100%;
 }
 
 nav {
